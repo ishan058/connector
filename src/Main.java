5a0913620f23aff58923808ebffdcfb89a61ce64
ishan058 <ishankhadkaik246@gmail.com>
2023-05-08 10:34:30 +0545

    prepared

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,22 @@ public class Main {
         Connection conn = DriverManager.getConnection(url,"root", "root");
         System.out.println("connection success");
 
-        //Patch data from the database;
-        Statement statement = conn.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * from user");
-        System.out.println(resultSet);
+        //Fetch data from the database;
+//        Statement statement = conn.createStatement();
+//        ResultSet resultSet = statement.executeQuery("SELECT * from user WHERE id + 1");
+////        System.out.println(resultSet);
+
+        //Prepare statement
+        PreparedStatement preparedStatement = conn.prepareStatement("SELECT * from user WHERE pid =? OR address=?");
+        preparedStatement.setInt(1,10);
+        preparedStatement.setString(2,"CHITWAN");
+
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
 
         while(resultSet.next()){
-            System.out.println(resultSet.getInt("id") + "");
+            System.out.println(resultSet.getInt("pid") + " ");
             System.out.println(resultSet.getString("name"));
             System.out.println(" ");
             System.out.println(resultSet.getString("address"));
